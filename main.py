e7622ec8e67714ab8002dd2c182b2a10323f589d
lucas <lucasmach@ufl.edu>
2024-04-11 12:00:45 -0400

    added decode

--- main.py
+++ main.py
@@ -5,6 +5,9 @@
 def encode():
     pass
 
+def decode():
+    pass
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
